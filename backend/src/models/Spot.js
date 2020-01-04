537e85e32a9d3301ed03315619d3ed4945f29ee0
Marcelo Oliveira <marcelo_alfahot@hotmail.com>
2020-01-04 13:50:05 -0300

    Fix bugs and initiate mobile folder
    Fix some bugs on the backend, and start the mobile project

--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -1,21 +1,24 @@
-const mongoose = require('mongoose');
-const SpotSchema = new mongoose.Schema({
+const mongoose = require("mongoose");
+const SpotSchema = new mongoose.Schema(
+  {
     thumbnail: String,
     company: String,
     price: Number,
     techs: [String],
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
     }
-}, {
+  },
+  {
     toJSON: {
-        virtuals: true,
+      virtuals: true
     }
+  }
+);
+
+SpotSchema.virtual("thumbnail_url").get(function() {
+  return `http://localhost:3333/files/${this.thumbnail}`;
 });
 
-SpotSchema.virtual('thumbnail_url').get(function() {
-    return `http://localhost:3333/files/${this.thumbnail_url}`
-})
-
-module.exports = mongoose.model('Spot', SpotSchema);
+module.exports = mongoose.model("Spot", SpotSchema);
