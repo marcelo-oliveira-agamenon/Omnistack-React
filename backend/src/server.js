d979af5327188cb6f1c5774294e4810ef6274a36
Marcelo Oliveira <marcelo_alfahot@hotmail.com>
2020-01-11 21:27:43 -0300

    Fixing bugs and starting socket

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,33 @@
-const express = require('express');
-const routes = require('./routes');
-const mangoose = require('mongoose');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const routes = require("./routes");
+const mangoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const socketio = require("socket.io");
+const http = require("http");
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
-mangoose.connect('mongodb+srv://marcelo:marcelo@omnistack-pvnlc.mongodb.net/test?retryWrites=true&w=majority', {
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user_id } = socket.handshake.query;
+  connectedUsers[user_id] = socket.id;
+});
+
+mangoose.connect(
+  "mongodb+srv://marcelo:1234@omnistack-pvnlc.mongodb.net/test?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true 
+    useUnifiedTopology: true
+  }
+);
+
+app.use((req, res, next) => {
+  (req.io = io), (req.connectedUsers = connectedUsers);
+  return next();
 });
 
 //GET, POST, PUT, DELETE
@@ -19,6 +38,6 @@ mangoose.connect('mongodb+srv://marcelo:marcelo@omnistack-pvnlc.mongodb.net/test
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 
-app.listen(3333);
+server.listen(3333);
--- frontend/src/pages/dashboard/index.js
+++ frontend/src/pages/dashboard/index.js
@@ -1,39 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import api from '../../services/api';
-import '../dashboard/styles.css';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import api from "../../services/api";
+import socketio from "socket.io-client";
+import "../dashboard/styles.css";
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
-    const [spot, setSpot] = useState([]);
-    
-    useEffect( () => {
-        async function loadSpots() {
-            const user_id = localStorage.getItem('user');
-            const response = await api.get('/dashboard', {
-                headers: { user_id }
-            });
+  const [spot, setSpot] = useState([]);
 
-            setSpot(response.data);
-        }
+  useEffect(() => {
+    const user_id = localStorage.getItem("user");
+    const socket = socketio("http://localhost:3333", {
+      query: { user_id }
+    });
+  }, []);
 
-        loadSpots();
-    }, []);
+  useEffect(() => {
+    async function loadSpots() {
+      const user_id = localStorage.getItem("user");
+      const response = await api.get("/dashboard", {
+        headers: { user_id }
+      });
 
-    return (
-        <>
-            <ul className="spot-List">
-                {spot.map(spot => (
-                    <li key={spot._id}>
-                        <header style={{ backgroundImage: `url(${spot.thumbnail.url})` }}/>
-                        <strong>{spot.company}</strong>
-                        <span>{spot.price ? `R$${spot.price}/dia` : 'GRATUITO'}</span>
-                    </li>
-                ))}
-            </ul>
+      setSpot(response.data);
+    }
 
-            <Link to="/New">
-                <button className="btnEntrar">Cadastrar novo spot</button>
-            </Link>
-        </>
-    )
+    loadSpots();
+  }, []);
+
+  return (
+    <>
+      <ul className="spot-List">
+        {spot.map(spot => (
+          <li key={spot._id}>
+            <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} />
+            <strong>{spot.company}</strong>
+            <span>{spot.price ? `R$${spot.price}/dia` : "GRATUITO"}</span>
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/New">
+        <button className="btnEntrar">Cadastrar novo spot</button>
+      </Link>
+    </>
+  );
 }
